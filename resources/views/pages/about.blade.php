2575d23955d512018ea0a275ed4c8e50ff86c20e
Nasir Khan <1nasir2khan3@gmail.com>
2016-12-31 01:50:18 +0600

    fixing about

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,7 +5,7 @@
 
         <div class="aside">
             <div class="region region-sidebar-second">
-<<<<<<< HEAD
+
     <div class="form aside-widget">
     <img alt="Build Muscle, Lose Fat &amp; Stay Motivated!" src="https://cdn.muscleandstrength.com/sites/all/themes/mnsnew/images/signupbanner.jpg">
     <div class="wrap">
@@ -30,103 +30,6 @@
                             <image width="45" height="45" class="muscle-down" src="https://cdn.muscleandstrength.com/images/fallback/muscle-icon.png"></image>
                             <image width="45" height="45" class="muscle-up" src="https://cdn.muscleandstrength.com/images/fallback/muscle-icon-2.png"></image>
                         </svg>
-=======
-                <div class="form aside-widget">
-                    <img alt="Build Muscle, Lose Fat &amp; Stay Motivated!" src="https://cdn.muscleandstrength.com/sites/all/themes/mnsnew/images/signupbanner.jpg">
-                    <div class="wrap">
-                        <p>Monster Gear weekly email newsletter sends you workouts, articles and motivation based on your goal.</p>
-
-                        <form method="post" action="" name="icpsignup" id="icpsignup6636" accept-charset="UTF-8" onsubmit="return verifyRequired6636();">
-                            <input name="redirect" value="" type="hidden">
-                            <input name="errorredirect" value="" type="hidden">
-                            <div class="qg-full">
-                                <input name="fields_fname" placeholder="first name" type="text">
-                            </div>
-                            <div class="qg-full">
-                                <input name="fields_email" placeholder="email" type="text">
-                            </div>
-
-                            <div class="qg-half">
-                                <button class="btn-icon-muscle btn btn-blue half" type="submit" onclick="ga('send', 'event', { eventCategory: 'email', eventAction: 'signup', eventLabel: 'sidebar'});">
-                                    <span class="text">Sign up</span>
-                                    <div class="muscle-icon">
-                                        <svg>
-                                            <use class="muscle-down" xlink:href="#icon-muscle-icon"></use>
-                                            <use class="muscle-up" xlink:href="#icon-muscle-icon-2"></use>
-                                            <image width="45" height="45" class="muscle-down" src="https://cdn.muscleandstrength.com/images/fallback/muscle-icon.png"></image>
-                                            <image width="45" height="45" class="muscle-up" src="https://cdn.muscleandstrength.com/images/fallback/muscle-icon-2.png"></image>
-                                        </svg>
-                                    </div>
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-
-                </div>
-
-
-
-                <div id="block-block-17" class="block block-block">
-
-
-                    <div class="content">
-                        <div style="position: relative; width: 302px; height: 370px; max-width: 100%;" class="radWrapper">
-                            <!-- MNS_Site_Sidebar_Top -->
-                            <div data-google-query-id="CMyqjovc7dACFcgQaAodPC0FuQ" id="div-gpt-ad-1423352102131-0" style="width: 302px; height: 370px; transform-origin: 0px 0px 0px; position: absolute; transform: scale(0.999);">
-
-                                <div style="border: 0pt none;" id="google_ads_iframe_/9172182/MNS_Site_Sidebar_Top_0__container__"><iframe srcdoc="" style="border: 0px none; vertical-align: bottom;" marginheight="0" marginwidth="0" scrolling="no" name="google_ads_iframe_/9172182/MNS_Site_Sidebar_Top_0" title="3rd party ad content" id="google_ads_iframe_/9172182/MNS_Site_Sidebar_Top_0" frameborder="0" height="370" width="302"></iframe></div><iframe srcdoc="" style="border: 0px none; vertical-align: bottom; visibility: hidden; display: none;" marginheight="0" marginwidth="0" scrolling="no" name="google_ads_iframe_/9172182/MNS_Site_Sidebar_Top_0__hidden__" title="" id="google_ads_iframe_/9172182/MNS_Site_Sidebar_Top_0__hidden__" frameborder="0" height="0" width="0"></iframe></div>
-                        </div>  </div>
-                </div>
-                <div id="block-mnsblock-mnssidebar-content" class="block block-mnsblock">
-
-
-                    <div class="content">
-                        <div class="h3">Must Read Articles</div>
-
-                        <ul class="popular-list">
-                            <li>
-                                <a href="/expert-guides/muscle-building" title="Complete Muscle Building Guide: Learn How To Build Muscle">
-                                    <img src="https://cdn.muscleandstrength.com/sites/default/files/styles/thumbnail_145/public/max-gym-450_0.jpg?itok=T0iWGKBF" alt="">
-                                    <span>Complete Muscle Building Guide: Learn How To Build Muscle</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/articles/50-unwritten-gym-rules" title="50 Unwritten Gym Rules Every Bro Should Know!">
-                                    <img src="https://cdn.muscleandstrength.com/sites/default/files/styles/thumbnail_145/public/bro.jpg?itok=-8FbDjoR" alt="">
-                                    <span>50 Unwritten Gym Rules Every Bro Should Know!</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/expert-guides/pre-workout" title="How To Maximize Results With Pre-Workout Supplements">
-                                    <img src="https://cdn.muscleandstrength.com/sites/default/files/styles/thumbnail_145/public/prewo.jpg?itok=y23I7Ap3" alt="">
-                                    <span>How To Maximize Results With Pre-Workout Supplements</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/articles/movie-muscle-38-greatest-male-hollywood-physiques" title="The 38 Greatest Male Hollywood Physiques Of All Time">
-                                    <img src="https://cdn.muscleandstrength.com/sites/default/files/styles/thumbnail_145/public/movie.jpg?itok=VYH32QEW" alt="">
-                                    <span>The 38 Greatest Male Hollywood Physiques Of All Time</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/expert-guides/whey-protein" title="The Essential Guide To Whey Protein Supplements">
-                                    <img src="https://cdn.muscleandstrength.com/sites/default/files/styles/thumbnail_145/public/suppreviews.jpg?itok=tY2p4GE7" alt="">
-                                    <span>The Essential Guide To Whey Protein Supplements</span>
-                                </a>
-                            </li>
-                        </ul></div>
-                </div>
-                <div id="block-block-9" class="block block-block">
-
-
-                    <div class="content">
-                        <div style="position: relative; width: 302px; height: 370px; max-width: 100%;" class="radWrapper">
-                            <!-- MNS_Site_Sidebar_Bottom -->
-                            <div data-google-query-id="CJqqkYvc7dACFc-kaAodfBIKHA" id="div-gpt-ad-1421356883797-0" style="width: 302px; height: 370px; transform-origin: 0px 0px 0px; position: absolute; transform: scale(0.999);">
-
-                                <div style="border: 0pt none;" id="google_ads_iframe_/9172182/MNS_Site_Sidebar_Bottom_0__container__"><iframe srcdoc="" style="border: 0px none; vertical-align: bottom;" marginheight="0" marginwidth="0" scrolling="no" name="google_ads_iframe_/9172182/MNS_Site_Sidebar_Bottom_0" title="3rd party ad content" id="google_ads_iframe_/9172182/MNS_Site_Sidebar_Bottom_0" frameborder="0" height="370" width="302"></iframe></div><iframe srcdoc="" style="border: 0px none; vertical-align: bottom; visibility: hidden; display: none;" marginheight="0" marginwidth="0" scrolling="no" name="google_ads_iframe_/9172182/MNS_Site_Sidebar_Bottom_0__hidden__" title="" id="google_ads_iframe_/9172182/MNS_Site_Sidebar_Bottom_0__hidden__" frameborder="0" height="0" width="0"></iframe></div>
-                        </div>
->>>>>>> 338837da1ca9f0c218e52798bafb19f81d51988e
                     </div>
                 </button>
             </div>
